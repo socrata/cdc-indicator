68bc13143a90e7462272cb1fa242be6c1ce3a15b
James Chuang <james.chuang@socrata.com>
2016-06-29 15:39:43 -0700

    CS-8225: add hot realod

---
+++ webpack-dev-server.js
@@ -0,0 +1,5 @@
+var config = require("./webpack.config.js");
+config.entry.app.unshift("webpack-dev-server/client?http://localhost:8080/");
+var compiler = webpack(config);
+var server = new WebpackDevServer(compiler, {...});
+server.listen(8080);
